8a02b073243d25c4cd9c624d8831ea8d58b9b9ce
sidneykm <skiyoshim@gmail.com>
2018-02-28 10:42:51 -0300

    'Arquivos_do_rpojeto_local'

---
+++ spec/exemplo/exemploSpec.js
@@ -0,0 +1,17 @@
+describe("Suite de teste de exemplo com NodeJS", function () {
+
+	it("deve demonstrar o uso do Jasmine com o NodeJS", function() {
+
+		expect(true).toEqual(true);
+
+	});
+
+	it("deve demonstrar o uso do karma em mode descuta", function (){
+
+
+		expect(1).toEqual(1);
+
+
+	});
+
+});
